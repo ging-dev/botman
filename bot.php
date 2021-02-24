b49aa3d9df69b8f69aadcccbaf80c173625582e9
ging-dev <gingdz11012001@gmail.com>
2021-02-24 20:41:24 +0700

    Add Hentai

--- bot.php
+++ bot.php
@@ -2,9 +2,12 @@
 
 // >_ 2021 Gingdev
 
+use Curl\Curl;
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\BotMan\Messages\Attachments\Image;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 require __DIR__.'/vendor/autoload.php';
 
@@ -19,6 +22,7 @@ $config = [
 ];
 
 $botman = BotManFactory::create($config);
+$curl   = new Curl();
 
 $botman->hears('hello', function (BotMan $bot) {
     $bot->reply('Hello yourself.');
@@ -26,6 +30,14 @@ $botman->hears('hello', function (BotMan $bot) {
 
 $botman->hears('dze', function (BotMan $bot) {
     $bot->reply('Bé Naga Tôn');
+});
+
+$botman->hears('hentai', function(BotMan $bot) use ($curl) {
+    $curl->get('https://nekos.life/api/v2/img/hentai');
+    $attachment = new Image($curl->response->url);
+    $message = OutgoingMessage::create('Dame bro!!!')
+        ->withAttachment($attachment);
+    $bot->reply($message);
 });
 
 $botman->listen();
